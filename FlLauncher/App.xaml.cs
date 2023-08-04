8dbc912ee5f09c37681f0a09e22ae6b1a05646e2
Глеб Гребенщиков <Glebka_1234@trainzcity.myftp.org>
2023-08-04 14:43:58 +0300

    Third Commit: Implemented a global exception handler

--- FlLauncher/App.xaml.cs
+++ FlLauncher/App.xaml.cs
@@ -12,5 +12,14 @@ namespace FlLauncher
     /// </summary>
     public partial class App : Application
     {
+        public App() {
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+            MessageBox.Show("Произошла критическая ошибка. Приложение постарается сохранить свою работу, но результат выполнения процесса не гарантируется. Рекомендуем повторить попытку заново.", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
     }
 }
