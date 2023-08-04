f5293030e7cbe70fcc0b14fb06c93d78189669e8
Глеб Гребенщиков <Glebka_1234@trainzcity.myftp.org>
2023-08-04 14:32:10 +0300

    Second Commit: Set filestructure

--- FlLauncher/Windows/MainWindow.xaml.cs
+++ FlLauncher/Windows/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace FlLauncher
+namespace FlLauncher.Windows
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
