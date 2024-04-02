a61e312112951a10418d8bdcc70dd6d2c17d0ab4
Gleb-pro-admin <62476577+Gleb-pro-admin@users.noreply.github.com>
2024-04-02 18:37:00 +0300

    Fix java 17

--- setup.sh
+++ setup.sh
@@ -2,17 +2,17 @@ apt install openssh-server -q -y
 sed '/PasswordAuthentication/d' /etc/ssh/sshd_config
 echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
 
-apt install default-jre-headless -qq
-apt install screen -qq
+apt install openjdk-17-jre -q -y
+apt install screen -q -y
 
 mkdir /srv/minecraft
 adduser --system --no-create-home --home /srv/minecraft minecraft
-chown minecraft:minecraft /srv/minecraft
+chown minecraft:nogroup /srv/minecraft
 
 cd /srv/minecraft
 wget https://download.getbukkit.org/spigot/spigot-1.20.1.jar
-su -u minecraft -c java -Xms2048M -Xmx4096M -jar spigot-1.20.1.jar nogui
+su minecraft -c java -Xms2048M -Xmx4096M -jar spigot-1.20.1.jar nogui
 sed '/eula/d' eula.txt
 echo eula=true > eula.txt
 
-su -u minecraft -c screen -S minecraft java -Xms2048M -Xmx4096M -jar spigot-1.20.1.jar nogui
+su minecraft -c screen -S minecraft java -Xms2048M -Xmx4096M -jar spigot-1.20.1.jar nogui
